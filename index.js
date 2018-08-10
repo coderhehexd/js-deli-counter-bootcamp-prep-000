c8727d81f761f363bae9f123d696c40c10f0f3a3
Chris Li <cli78822@gmail.com>
2018-08-10 05:52:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ var nowServing = (line) =>{
     return `Currently serving ${serving}.`
   }
 };
+
+var currentLine = (line)=>{
+  if(line.length === 0){
+    return "The line is currently empty."
+  }else{
+    var online = "The line is currently: "
+    for(let i = 0; i < line.length; i++){
+      online += `${i}. ${line[i]}`
+    }
+    return online
+  }
+}
