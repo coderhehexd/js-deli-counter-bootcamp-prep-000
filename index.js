90d13de005642331ab8cdd798119ebe4ac9f2b73
Chris Li <cli78822@gmail.com>
2018-08-10 05:53:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var currentLine = (line)=>{
     var online = "The line is currently: "
     for(let i = 0; i < line.length; i++){
       online += `${i+1}. ${line[i]}`
-      if(i !+ line.length-1){
+      if(i != line.length-1){
         online+=","
       }
     }
