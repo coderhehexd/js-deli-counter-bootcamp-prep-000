411033c4ff8a56663ce7dab6562389cbc5c54f65
Chris Li <cli78822@gmail.com>
2018-08-10 05:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ var takeANumber = (line, name) =>{
   return `Welcome, ${name}. You are number ${line.length } in line.`
 };
 
-var nowServer = 
+var nowServer = (line) =>{
+  if(line.length === 0){
+    return "There is nobody waiting to be served!";
+  }else{
+    var serving = line[0]
+    line.shift()
+    return serving
+  }
+};
